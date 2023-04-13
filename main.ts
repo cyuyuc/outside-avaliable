4ec2654c59343ee556682ae8cd5040b6cffffc7d
cyuyuc <130678477+cyuyuc@users.noreply.github.com>
2023-04-13 23:14:34 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,17 @@
+radio.onReceivedValue(function (name, value) {
+    available = value
+})
+let available = 0
+radio.setGroup(11)
+radio.setTransmitPower(7)
+basic.forever(function () {
+    if (available == 1) {
+        basic.showIcon(IconNames.Happy)
+    } else if (available == 2) {
+        basic.showIcon(IconNames.Sad)
+    } else {
+        basic.showString("error")
+        music.playMelody("C5 A B G A F G E ", 120)
+    }
+    available = 0
+})
